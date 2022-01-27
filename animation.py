c93a2c5760c7dfb5093b995c094b1dc201cfa63d
rmn@mbp <rohoehn123@gmail.com>
2022-01-28 00:26:27 +0100

    getting there

--- animation.py
+++ animation.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def f(x,a):
-    return a * (x ** 2)
+    return a * (x)
 
 
 def draw(fun, x, a, axes):
@@ -16,15 +16,15 @@ if __name__ == "__main__":
     ax.set_xlabel("x")
     ax.set_ylabel("y")
 
-    ax.se
+    ax.set_xlim(-100, 100)
+    ax.set_ylim(-100, 100)
 
     x = np.linspace(-100, 100, 100)
 
-    for n in range(1, 11):
-        a = n / 100
+    for a in range(1, 11):
         draw(f, x, a, ax)
         plt.draw()
-        plt.pause(.1)
+        plt.pause(1.0)
         plt.cla()
 
         
